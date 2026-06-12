ab7e697171fd153eb4f0c47deb4b4bc24205d8a4
Ajaj Anwar Anik <ajaj.anik180@gmail.com>
2026-06-12 19:40:39 +0600

    added Jframe

--- src/JFrame/JFrame.java
+++ src/JFrame/JFrame.java
@@ -5,20 +5,35 @@ import java.awt.*;
 
 public class JFrame {
     public static void main(String[] args) {
+        javax.swing.JFrame f1 = new javax.swing.JFrame("Checker");;
 
-        javax.swing.JFrame frame =
-                new javax.swing.JFrame("Anik's Frame");
+        JTextField t = new JTextField(4);
+        JLabel a = new JLabel("Enter 1st number");
+        JLabel a1 = new JLabel("Enter 2st number");
+        JButton b = new JButton("ADD");
+        JLabel j = new JLabel("Result");
+        JTextField t1 = new JTextField(4);
+        JButton b1 = new JButton("SUB");
+        JButton b2 = new JButton("DIV");
+        JButton b4 = new JButton("MUL");
 
-        frame.setSize(400, 400);
-        frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
+        f1.setLayout(new FlowLayout());
 
-        frame.getContentPane().setBackground(Color.PINK);
+        f1.add(a);
+        f1.add(t);
+        f1.add(a1);
+        f1.add(t1);
+        f1.add(b);
+        f1.add(b1);
+        f1.add(b2);
+        f1.add(b4);
+        f1.add(j);
 
-        JLabel label = new JLabel("Welcome, Ajaj Anwar Anik!", SwingConstants.CENTER);
-        label.setFont(new Font("Arial", Font.BOLD, 16));
-        frame.add(label);
-
-        frame.setVisible(true);
+        f1.getContentPane().setBackground(Color.PINK);
+        
+        f1.setSize(250, 300);
+        f1.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        f1.setLocationRelativeTo(null);
+        f1.setVisible(true);
     }
 }
